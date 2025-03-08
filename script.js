f810aa1b8489ac33587b313be8dbdf30cd4cd9b9
Reed Stevens <Reed.El.Stevens@gmail.com>
2025-03-08 12:26:49 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,11 @@
 let testNum = 1;
 
 function buttonFunction() {
-  let addStuffDiv = document.querySelector('#addStuffHere');
-  for (let x = 0; x < testNum; x++) {
-    addStuffDiv.appendChild(document.createElement('p')).textContent='Test Added!';
-  }
+  // let addStuffDiv = document.querySelector('#addStuffHere');
+  // for (let x = 0; x < testNum; x++) {
+  //   addStuffDiv.appendChild(document.createElement('p')).textContent='Test Added!';
+  // }
+  testFunc();
 }
 
 function callbackFunc(response) {
