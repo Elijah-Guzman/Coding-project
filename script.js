3351610e4c0a4fd4631042e2554ce511de4bc5d6
Reed Stevens <Reed.El.Stevens@gmail.com>
2025-03-07 23:16:21 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 let testNum = 1;
 
 function buttonFunction() {
+  let addStuffDiv = document.querySelector('#addStuffHere');
   for (let x = 0; x < testNum; x++) {
-    let addStuffDiv = document.querySelector('#addStuffHere');
     addStuffDiv.appendChild(document.createElement('p')).textContent='Test Added!';
   }
 }
